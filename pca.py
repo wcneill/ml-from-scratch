6702fa2685ba75867fe2c5eee1624a3b3a708f0f
Wesley Neill <wesley.neill@gmail.com>
2020-03-10 09:57:13 -0700

    added more jupyter stuff

--- pca.py
+++ pca.py
@@ -1,15 +1,70 @@
 import seaborn as sb
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
 
-# get n samples of m-dimensional data (vectors x_1 to x_n)
+if __name__ == '__main__':
 
-S = np.array([[95, 1], [1, 5]])
+    # get variable names
+    cols = ['sepal length', 'sepal width', 'petal length', 'petal width', 'classification']
 
-vals, vecs = np.linalg.eig(S)
+    # PCA Step 1: get n samples of m-dimensional data (sample vectors s_1 to s_n, which hare samples of variables
+    # x_1 to x_m)
 
-print(vals)
-print(vecs)
+    # read data (long form, for plotting in seaborn):
+    df = pd.read_csv('data/iris.data', names=cols)
 
-# compute the mean mu of said vectors
+    # set classification column data type to string
+    df['classification'] = df['classification'].astype("string")
+
+    # set index by classification for easier access to subsets of the data
+    df.set_index('classification', inplace=True, drop=False)
+    classification = df.pop('classification')
+    print(df)
+
+    # set figure size:
+    palette = sb.color_palette("husl", 3)
+    x, y = ("petal length", "petal width")
+    sb.relplot(x=x, y=y, data=df, hue=classification, palette=palette)
+    sb.lmplot(x=x, y=y, data=df, palette=palette)
+    # plt.show()
+
+
+    # PCA Step 2: compute the average of each variable
+    mean = df.mean(0) # average of each column (average of each variable)
+
+    # PCA Step 3: We have the average of each variable in a row vector, now subtract it from each row.
+    Z = df.apply(lambda x: x - mean, axis=1).to_numpy()
+
+    # PCA Step 4: find the covariance matrix (1/n)(Z.T * Z):
+    C = (1 / (len(Z) - 1)) * (Z.T @ Z)
+
+    # PCA Step 5: find the eigenvalues and eigenvectors of the covariance matrix C
+    vals, vects = np.linalg.eig(C)
+
+    # PCA Step 5: sort the eigenvalues and eigenvectors in decreasing order:
+    idx = np.argsort(vals)[::-1]  # returns a list of indices that represent descending order of eigenvalues.
+    vals = vals[idx]
+    vects = vects[:, idx]
+
+    # PCA Step 6: Compose matrix P of columns made from sorted eigenvectors (C = PDP^(-1)):
+    # This step is already done, as numpy returns eigenvectors as a list of lists, where the eigenvectors are in the
+    # columns.
+
+    # Step 7: Reconstitute our original data by multiplying it by our matrix P:
+    Z_prime = Z @ vects
+
+    
+
+
+
+
+
+
+
+
+
+
 
 # Build the matrix B, which is the column matrix comprized of those vectors adjusted by the mean
 
